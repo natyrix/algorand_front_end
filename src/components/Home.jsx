bf00e46f3316a5bfb65529ed4450fecf14646f91
Natnael Melese <natrix277@gmail.com>
2022-09-28 19:14:35 -0400

    AlgSign init

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,35 +1,71 @@
+/* global AlgoSigner */
 import React, {useState, useEffect}  from 'react'
 import axios from 'axios'
+import './Home.css'
 import MyAlgoConnect from '@randlabs/myalgo-connect';
 import loading from '../loading2.gif';
+
+
 
 
 function Home() {
     const [response, setResponse] = useState({})
     const [isLoading, setIsLoading] = useState(false)
+    const [isAccountSelected, setIsAccountSelected] = useState(false)
+    const [accounts, setAccounts] = useState([])
+    const [selectedAccount, setSetselectedAccount] = React.useState('');
+
 
     useEffect(()=>{
         setIsLoading(true)
-        connectWallet()
+        getAccounts()
         setIsLoading(false)
       },[])
 
-    async function connectWallet(){
-        const myAlgoConnect = new MyAlgoConnect();
-        const acc = await myAlgoConnect.connect();
-        console.log(acc[0].address)
+    async function getAccounts(){
+        await AlgoSigner.connect({
+            ledger: 'TestNet'
+          });
+        const accts = await AlgoSigner.accounts({
+            ledger: 'TestNet'
+          });
+        let fetchedAccounts = accts
+        // console.log(fetchedAccounts)
+        // console.log(fetchedAccounts.length)
+        let ac = []
+        for(let i=0;i<fetchedAccounts.length;i++){
+            ac.push(fetchedAccounts[i].address)
+            // console.log(fetchedAccounts[i].address)
+        }
+        // console.log(ac)
+        setAccounts([])
+        setAccounts(ac)
     }
+
+    const Dropdown = ({ label, value, options, onChange })=>{
+        return (
+            <label>
+              {label}
+              <select value={value} onChange={onChange}>
+                {options.map((option) => (
+                  <option value={option} key={option}>{option}</option>
+                ))}
+              </select>
+            </label>
+          );
+    }
+
+    const handleChange = (event) => {
+        setSetselectedAccount(event.target.value);
+    };
+
+
     return (
         <>
-      {/* <h1>My Application</h1> */}
-      {isLoading 
-?<img src={loading}></img>:<div>Home</div>}
-    </>
-        
-    //     {isLoading
-    //   <img src={loading}></img>
-    //         :<div>Home</div>}
-        
+            {isLoading &&
+                <img src={loading}></img>}
+            {accounts.length > 0 && (<Dropdown label="Select accout" value={selectedAccount} onChange={handleChange} options={accounts}/>)}
+        </>
         
     )
 }
